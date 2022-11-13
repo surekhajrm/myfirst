8c072931d0b31e2c4a581de3779db3bb19318157
Bhuvikap <surekhajrm@gmail.com>
2022-11-14 01:49:15 +0530

    intial commit

--- src/test/java/Devtestclass.java
+++ src/test/java/Devtestclass.java
@@ -3,6 +3,8 @@ public class Devtestclass {
     Public static void main(string[] args){
 
       System.out.println("IT s a devtestclass") ;
+      //surekha added this
+        System.out.println("It's update");
     }
 
 }
